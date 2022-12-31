e82053bb83487ba6d45a122bbf38540d4ad24143
Dany Reyna <luisd.reyna@outlook.com>
2022-12-31 15:59:19 -0600

    fix: await queries

--- remix/app/routes/index.tsx
+++ remix/app/routes/index.tsx
@@ -2,9 +2,13 @@ import {json} from "@remix-run/node";
 import {db} from "~/utils/db.server";
 
 export async function loader() {
+  const orders = await db.orders.findMany();
+  const products = await db.products.findMany();
+  const orderDetails = await db.order_Details.findMany();
+
   return json({
-    orders: db.orders.findMany(),
-    products: db.products.findMany(),
-    orderDetails: db.order_Details.findMany(),
+    orders,
+    products,
+    orderDetails,
   });
 }
