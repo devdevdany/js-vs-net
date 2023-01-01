2c7824f26bb53d829efe1d244c263124ec0497b5
Dany Reyna <luisd.reyna@outlook.com>
2022-12-31 18:39:09 -0600

    feat(remix): inline queries

--- remix/app/routes/index.tsx
+++ remix/app/routes/index.tsx
@@ -2,27 +2,16 @@ import {json} from "@remix-run/node";
 import {db} from "~/utils/db.server";
 
 export async function loader() {
-  const albums = await db.album.findMany();
-  const artists = await db.artist.findMany();
-  const customers = await db.customer.findMany();
-  const employees = await db.employee.findMany();
-  const genres = await db.genre.findMany();
-  const invoices = await db.invoice.findMany();
-  const invoiceLines = await db.invoiceLine.findMany();
-  const mediaTypes = await db.mediaType.findMany();
-  const playlists = await db.playlist.findMany();
-  const tracks = await db.track.findMany();
-
   return json({
-    albums,
-    artists,
-    customers,
-    employees,
-    genres,
-    invoices,
-    invoiceLines,
-    mediaTypes,
-    playlists,
-    tracks,
+    albums: await db.album.findMany(),
+    artists: await db.artist.findMany(),
+    customers: await db.customer.findMany(),
+    employees: await db.employee.findMany(),
+    genres: await db.genre.findMany(),
+    invoices: await db.invoice.findMany(),
+    invoiceLines: await db.invoiceLine.findMany(),
+    mediaTypes: await db.mediaType.findMany(),
+    playlists: await db.playlist.findMany(),
+    tracks: await db.track.findMany(),
   });
 }
